d7ad6ffa52e040455ae9582dd7a550d11faa7656
George Geddes <georgegdds@gmail.com>
2015-03-26 12:21:21 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+  name = 'mcrt',
+  packages = ['mcrt'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python implementation of the markov chain solution to the radiative transfer problem.',
+  author = 'George Geddes',
+  author_email = 'georgegdds@gmail.com',
+  keywords = ['RT','radiative','transfer','markov','scattering'], # arbitrary keywords
+  classifiers = [],
+)
