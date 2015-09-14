8879ff0efd79d19fb34d7e9559540e527983249a
George Geddes <George_Geddes@student.uml.edu>
2015-09-14 15:15:13 -0400

    Gas and related stuff now provided by mcrt/gas.py

--- mcrt/mcrt.py
+++ mcrt/mcrt.py
@@ -1,68 +1,9 @@
 from __future__ import division
 import numpy as np
 import scipy.integrate, scipy.special
+from .gas import Gas, lineshape, sdu, frequency, freq_param, kB, c
 
 dt = np.float
-
-# Boltzmann's constant
-kB = 1.3806488e-23 # J / K
-
-# speed of light
-c = 2.99792458e8 # m / s
-
-def freq_param( num_freqs=24 ):
-    """Frequency parameter x = ( nu - nu_0 ) / nu_0"""
-    # the 1e-6 padding is to prevent division errors in the extinction coefficients
-    return np.array( [ 2.25 * (i + 1e-6) / num_freqs for i in xrange(num_freqs-1) ] + [ np.inf ], dtype=dt ).reshape( 1, num_freqs )
-
-def frequency( wavelength ):
-    """Changes wavelength to frequency"""
-    return c / wavelength
-
-def sdu( m, T ):
-    """Doppler unit for mass `m` and temperature `T`.
-    The standard deviation for a doppler-broadened line centered at 'nu' is `sdu( m, T ) * nu` """
-    return np.sqrt( kB * T / m / c**2 )
-
-class lineshape( object ):
-    def __init__( self, mean, std, prof_type='doppler' ):
-        self.mean = mean
-        self.std  = std
-        self.prof_type = prof_type
-
-    @property
-    def cdf( self ):
-        """Returns an array of the cdf"""
-        return scipy.special.erf( freq_param() / self.std / np.sqrt(2) )
-
-class Gas( object ):
-    """Hold cross section info for each species.
-    `Gas.sigma(T)` will return the temperature-dependent cross sections at T kelvin"""
-    species = {}
-    absorbers = {}
-    scatterers = {}
-    def __init__(self, name, role, wavelengths, cross_sections, mass ):
-        """
-        `role`           = 'absorption' or 'scattering'
-        `wavelengths`    = wavelengths corresponding to `cross_sections`
-        `cross_sections` = reference cross sections at 1000 K
-        """
-        self.name = name
-        self.role = role
-        self.wavelengths = wavelengths
-        self.mass = mass
-        if role == 'absorption':
-            # self.sigma = [ ( lambda T: s ) for s in cross_sections ]
-            self.sigma = cross_sections
-            self.species[name] = self
-            self.absorbers[name] = self
-        elif role == 'scattering':
-            # self.sigma = [ ( lambda T: s * np.sqrt( 1000 / T )  ) for s in cross_sections ]
-            self.sigma = cross_sections
-            self.scatterers[name] = self
-            self.species[name] = self
-        else:
-            raise Exception( "role {} not recognized".format( role ) )
 
 class Atmosphere( object ):
     """Contains atmospheric properties and methods for calculating RT."""
